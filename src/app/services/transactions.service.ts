617a73de3eaf0992f0f3ec41936cb41b209b7706
Camille Jackson <camillejackson205@gmail.com>
2018-09-26 15:57:39 -0400

    changes to transaction service

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Api_Url } from '../../environments/environment.prod';
-import { transaction } from '../models/Transactions';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +15,5 @@ export class TransactionsService {
 
   private getHeaders(){
     return new HttpHeaders().set('Authorization', `Bearer $${localStorage.getItem('id._token')}`);
-  }
-
-  createTransaction(transaction: transaction){
-    return this.http.post(`${Api_Url}/Transactions`, transaction, { headers: this.getHeaders()});
   }
 }
