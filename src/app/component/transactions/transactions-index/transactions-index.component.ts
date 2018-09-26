2902854363f71cf4462752a665aee637bf4c8d52
Camille Jackson <camillejackson205@gmail.com>
2018-09-26 14:58:02 -0400

    model naming convention fixed

--- src/app/component/transactions/transactions-index/transactions-index.component.ts
+++ src/app/component/transactions/transactions-index/transactions-index.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionsService } from '../../../services/transactions.service';
-import { transaction } from '../../../models/Transactions';
+import { transaction } from '../../../models/transactions';
 import { MatTableDataSource } from '@angular/material';
 
 @Component({
