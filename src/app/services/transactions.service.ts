b69219b7113755a3b36b3060a6cb7b314dfd0f6e
Camille Jackson <camillejackson205@gmail.com>
2018-09-26 15:41:24 -0400

    starting create CRUD

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Api_Url } from '../../environments/environment.prod';
+import { transaction } from '../models/Transactions';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,9 @@ export class TransactionsService {
 
   private getHeaders(){
     return new HttpHeaders().set('Authorization', `Bearer $${localStorage.getItem('id._token')}`);
+  }
+
+  createTransaction(transaction: transaction){
+    return this.http.post(`${Api_Url}/Transactions`, transaction, { headers: this.getHeaders()});
   }
 }
