ed02ff61816cf1b9db775af55560424eaae45cad
Joshua Tucker <thejltucker@gmail.com>
2018-09-27 14:05:57 -0400

    Fixed api link for products

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -11,7 +11,7 @@ export class ProductsService {
   constructor(private _http: HttpClient) { }
 
   getProducts() {
-    return this._http.get(`${Api_Url}/Products`, { headers: this.getHeaders() });
+    return this._http.get(`${Api_Url}api/Product`, { headers: this.getHeaders() });
 }
 
 private getHeaders() {
