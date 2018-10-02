143475d6a36d22238cc4671ad4ee97479049034c
Joshua Tucker <thejltucker@gmail.com>
2018-10-02 10:54:35 -0400

    Added image to product display

--- src/app/component/product/product-index/product-index.component.ts
+++ src/app/component/product/product-index/product-index.component.ts
@@ -3,7 +3,12 @@ import { ProductsService } from '../../../services/products.service';
 import { Product } from '../../../models/Product';
 import { MatTableDataSource } from '../../../../../node_modules/@angular/material';
 
-export interface Food {
+export interface Products {
+  value: string;
+  viewValue: string;
+}
+
+export interface Numbers {
   value: string;
   viewValue: string;
 }
@@ -16,24 +21,27 @@ export interface Food {
 
 export class ProductIndexComponent implements OnInit {
 
-  foods: Food[] = [
-    {value: 'Pink-0', viewValue: 'Pink'},
-    {value: 'Blue-1', viewValue: 'Blue'},
-    {value: 'Red-2', viewValue: 'Red'}
+  products: Products[] = [
+    { value: 'Pink-0', viewValue: 'Pink' },
+    { value: 'Blue-1', viewValue: 'Blue' },
+    { value: 'Red-2', viewValue: 'Red' }
   ];
-       Quantity: Food[] = [
-        {value: '0-0', viewValue: '0'},
-        {value: '1-1', viewValue: '1'},
-        {value: '2-2', viewValue: '2'}
-      ];
+  numbers: Numbers[] = [
+    { value: '1-0', viewValue: '1' },
+    { value: '2-1', viewValue: '2' },
+    { value: '3-2', viewValue: '3' },
+    { value: '4-3', viewValue: '4' },
+    { value: '5-4', viewValue: '5' },
+    { value: '6-5', viewValue: '6' }
+  ];
 
   constructor(private _productService: ProductsService) { }
   ngOnInit() {
 
-    this._productService.getProducts().subscribe((products: Product[]) => {      
+    this._productService.getProducts().subscribe((products: Product[]) => {
       this.dataSource = new MatTableDataSource<Product>(products);
     });
   }
-  columnNames = ['details', 'Name', 'Price', 'Description', 'buttons'];
+  columnNames = ['details', 'Image', 'Name', 'Price', 'Description', 'buttons'];
   dataSource: MatTableDataSource<Product>
 }
